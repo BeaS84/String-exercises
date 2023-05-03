0ad2c5a58189a90febb1922a92261482235efb96
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-05-03 21:23:43 +0200

    Third task solved

--- Strings/src/main/java/org/example/App.java
+++ Strings/src/main/java/org/example/App.java
@@ -1,15 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         //Zad 1. Kalkulator: napisz program kalkulator, który wspiera działania dodawania, odejmowania, mnożenia i dzielenia.
         // Użytkownik wpisuje działanie, np 4 + 5 i otrzymuje wynik: 4 + 5 = 9.
 
@@ -17,7 +15,7 @@ public class App
         System.out.println("Napisz równanie - oddziel spacją znak od liczby");
         String response = scanner.nextLine();
 
-        String[]numberSplit = response.split(" ");
+        String[] numberSplit = response.split(" ");
         //wyswietlenie elementów tablicy
 //        for (String element:numberSplit) {
 //            System.out.println(element);
@@ -59,23 +57,22 @@ public class App
                 break;
         }
 
-       // Zad 2. Odkurzacz do spacji: napisz program, który oczyszcza dany tekst ze wszystkich nadmiarowych spacji, pozostawiając tylko pojedyncze między słowami.
+        // Zad 2. Odkurzacz do spacji: napisz program, który oczyszcza dany tekst ze wszystkich nadmiarowych spacji, pozostawiając tylko pojedyncze między słowami.
         // Np Ala   ma           kota                 -> Ala ma kota
         Scanner scan = new Scanner(System.in);
         System.out.println(" Napisz pangramPhrase zawierające wiele spacji a ja oczyszcze dany tekst ze wszystkich nadmiarowych spacji, pozostawiając tylko pojedyncze między słowami. ");
-       String phrase = scan.nextLine();
+        String phrase = scan.nextLine();
 
-       //tworzymy wynikowy string
+        //tworzymy wynikowy string
         String text2 = "";
 
-        for (int i = 0; i<phrase.length();i++){
-            if(i==phrase.length()-1){
-                text2+=phrase.charAt(i);
-            } else if (phrase.charAt(i)==' '&& phrase.charAt(i+1)==' ') {
+        for (int i = 0; i < phrase.length(); i++) {
+            if (i == phrase.length() - 1) {
+                text2 += phrase.charAt(i);
+            } else if (phrase.charAt(i) == ' ' && phrase.charAt(i + 1) == ' ') {
                 continue;
-            }
-            else {
-                text2+= phrase.charAt(i);
+            } else {
+                text2 += phrase.charAt(i);
             }
         }
         System.out.println(text2.trim());
@@ -152,21 +149,54 @@ public class App
         boolean isPanagram = true;
         Integer counter = 0;
 
-        for (Integer i = 0; i<alphabet.length;i++){
-            if(!pangramPhrase.contains(alphabet[i].toString())){
+        for (Integer i = 0; i < alphabet.length; i++) {
+            if (!pangramPhrase.contains(alphabet[i].toString())) {
                 System.out.println("nie zawiera!");
                 isPanagram = false;
                 break;
             }
         }
-        if(isPanagram){
+        if (isPanagram) {
             System.out.println("The sentence is a pangramem");
-        }
-        else{
+        } else {
             System.out.println("The sentence is NOT a pangramem");
         }
 
+        //anagram
+        String string1 = "Tom Marvolo Riddle";
+        string1 = string1.trim().toLowerCase().replace(" ", "");
+        System.out.println(string1);
+        String string2 = "I am Lord Voldemort";
+        string2 = string2.trim().toLowerCase().replace(" ", "");
+        System.out.println(string2);
 
+        char[] array1 = string1.toCharArray();
+        char[] array2 = string2.toCharArray();
+        boolean isAnagram = true;
+
+        //sortowanie tablicy
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                if (array2[i] != array1[i]) {
+                    isAnagram = false;
+                    break;
+                }
+            }
+        } else {
+            isAnagram = false;
+        }
+
+        if (isAnagram) {
+            System.out.println("This is an anagram");
+        } else {
+            System.out.println("This is NOT an anagram");
+        }
     }
 
+
 }
+
+
