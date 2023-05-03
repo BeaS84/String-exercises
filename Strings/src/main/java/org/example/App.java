af83ccf01119e8274b39f24ca30260c990319f5b
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-05-03 20:24:26 +0200

    Second task solved

--- Strings/src/main/java/org/example/App.java
+++ Strings/src/main/java/org/example/App.java
@@ -59,10 +59,28 @@ public class App
                 break;
         }
 
-        
        // Zad 2. Odkurzacz do spacji: napisz program, który oczyszcza dany tekst ze wszystkich nadmiarowych spacji, pozostawiając tylko pojedyncze między słowami.
         // Np Ala   ma           kota                 -> Ala ma kota
-       // Zad 3. Analiza stringów: napisz program, który sprawdza, czy
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Napisz zdanie zawierające wiele spacji a ja oczyszcze dany tekst ze wszystkich nadmiarowych spacji, pozostawiając tylko pojedyncze między słowami. ");
+       String phrase = scan.nextLine();
+
+       //tworzymy wynikowy string
+        String text2 = "";
+
+        for (int i = 0; i<phrase.length();i++){
+            if(i==phrase.length()-1){
+                text2+=phrase.charAt(i);
+            } else if (phrase.charAt(i)==' '&& phrase.charAt(i+1)==' ') {
+                continue;
+            }
+            else {
+                text2+= phrase.charAt(i);
+            }
+        }
+        System.out.println(text2.trim());
+
+        // Zad 3. Analiza stringów: napisz program, który sprawdza, czy
         // dany string jest palindromem (odczytuje się go tak samo od przodu, jak i od tyłu, np Anna),
         // heterogramem (żadna litera się nie powtarza, np Mateusz),
         // pangramem (zawiera wszystkie litery alfabetu łacińskiego, np The quick brown fox jumps over a lazy dog),
